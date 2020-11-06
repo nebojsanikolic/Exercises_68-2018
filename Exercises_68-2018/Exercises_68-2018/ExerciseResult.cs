b901abe82d222c530aea55bf0bc41c072dc2c7ff
Nebojša Nikolć <nikolic.nebojsa.va@gmail.com>
2020-11-06 19:09:23 +0100

    Commit 3: Model klasa

---
+++ Exercises_68-2018/Exercises_68-2018/ExerciseResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_68_2018
+{
+    class ExerciseResult
+    {
+        public ExerciseResult(int id, string studentName, string studentIndex, int studentPoints)
+        {
+            Id = id;
+            StudentName = studentName;
+            StudentIndex = studentIndex;
+            StudentPoints = studentPoints;
+        }
+
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int StudentPoints { get; set; }
+    }
+}
